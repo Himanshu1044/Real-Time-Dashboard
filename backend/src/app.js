9651950907671c1ab5f021698d6cd27f2acc35e6
Himanshu1044 <swamihimanshu78@gmail.com>
2026-04-23 19:37:46 +0530

    Added frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors'
 import db from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
@@ -8,14 +10,18 @@ import eventRoutes from './routes/eventRoutes.js'
 
 const app = express();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('api is running')
-})
+// serve frontend
+app.use(express.static(path.join(__dirname, '../../frontend/views')));
+// console.log(path.join(__dirname, '../../frontend/views'))
 
+// routes
 app.use('/api/auth', authRoutes)
 app.use('/api/test', testRoutes)
 app.use('/api/events', eventRoutes)
